a230f0a542f34ff4c27983132f43494f312515c9
Boby Jamis Hari Sel <bj.harisel@gmail.com>
2018-01-11 09:45:42 +0700

    Update kelompok3.py

--- kelompok3.py
+++ kelompok3.py
@@ -1,1 +1,10 @@
-
+\section{ class and style object}
+\subsection{}
+Pada tahun 1993, tim National Center for Supercomputing Applications (NCSA) menulis spesifikasi untuk memanggil executable command line di milis www-talk. [2] [3] [4] Pengembang server Web lainnya menggunakannya, dan telah menjadi standar untuk server Web sejak saat itu. Sebuah kelompok kerja yang dipimpin oleh Ken Coar dimulai pada bulan November 1997 untuk mendapatkan definisi NCSA tentang CGI yang lebih formal. [5] Karya ini menghasilkan RFC 3875, yang menentukan Versi CGI 1.1. Secara khusus disebutkan di RFC adalah kontributor berikut: [6]
+\begin{itemize}
+\item Rob McCool (penulis Server Web HTTP NCSA)
+\item John Franks (penulis GN Web Server)
+\item Ari Luotonen (pengembang CERN httpd Web Server)
+\item Tony Sanders (penulis Web Server Plexus)
+\item George Phillips (pengelola server Web di University of British Columbia)
+\end{itemize}
